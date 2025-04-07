0dd1aa1598d57cf57c3c9f7507bfd332fc68bb31
alex <peter.alex.morris@gmail.com>
2025-04-07 23:06:06 +0900

    Output task titles is working

--- practice/practice/src/ToDo.java
+++ practice/practice/src/ToDo.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.*;
 
@@ -9,20 +10,28 @@ class Task {
         this.title = title;
         completion = false;
     }
+}
 
-    public void getTask() {
-        System.out.println(title);
+class TaskManager {
+    ArrayList<Task> tasks;
+
+    public TaskManager() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task newTask) {
+        tasks.add(newTask);
     }
 }
 
-class fileReader {
+class FileReading {
     String fileName;
 
-    public fileReader(String filename) {
+    public FileReading(String filename) {
         fileName = filename;
     }
 
-    public void makeFile(String filename) {
+    public void makeFile() {
         File output = new File("toDo.txt");
         try {
             output.createNewFile();
@@ -32,23 +41,69 @@ class fileReader {
     }
 }
 
+class FileWriting {
+    String fileName;
+
+    public FileWriting(String filename) {
+        fileName = filename;
+    }
+
+    public FileWriter makeFileWriter(String filename) {
+        FileWriter fileWrite;
+        try {
+            fileWrite = new FileWriter(filename);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return fileWrite;
+    }
+
+    public void writeToFile(ArrayList<Task> tasks, FileWriter writer) {
+        for (Task task : tasks) {
+            try {
+                writer.write(task.title);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}
+
 public class ToDo {
     public static void main(String[] args) {
         Scanner menuInput = new Scanner(System.in);
         Scanner taskInput = new Scanner(System.in);
 
-        fileReader newFile = new fileReader("toDo.txt");
-        newFile.makeFile(newFile.fileName);
+        // Make our task manager to hold outstanding tasks
+        TaskManager myManager = new TaskManager();
+
+        // Make a new file
+        FileReading myFile = new FileReading("toDo.txt");
+        myFile.makeFile();
+
+        // Make a file writer
+        FileWriting myWriter = new FileWriting("toDo.txt");
+        FileWriter fileWriter = myWriter.makeFileWriter(myWriter.fileName);
+        myWriter.writeToFile(myManager.tasks, fileWriter);
+
+        System.out.println("0. Save and quit\n1. Add Task");
 
         while (true) {
             int input = menuInput.nextInt();
-
             switch (input) {
                 case 0:
+                    myWriter.writeToFile(myManager.tasks, fileWriter);
+                    try {
+                        fileWriter.close();
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
                     return;
                 case 1:
+                    System.out.println("Task Title: ");
                     String taskName = taskInput.nextLine() + "\n";
                     Task newTask = new Task(taskName);
+                    myManager.addTask(newTask);
             }
         }
     }
